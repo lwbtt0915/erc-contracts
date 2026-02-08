17bdbddb4de129486e3fc49898a5c072e1edbc14
minmeizi <minmz830@tydic.com>
2026-02-08 10:49:27 +0800

    modify

--- script/Deploy.s.sol
+++ script/Deploy.s.sol
@@ -1,16 +1,22 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
-import "forge-std/Script.sol";
-import "../src/MyERC20.sol";
-import "../src/MyERC721.sol";
+import {Script} from "forge-std/Script.sol";
+import {MyERC20} from "../src/MyERC20.sol";
+import {MyERC721} from "../src/MyERC721.sol";
+import {console} from "forge-std/console.sol";
+
 
 contract Deploy is Script {
     function run() external {
         vm.startBroadcast();
 
         MyERC20 erc20 = new MyERC20("MyToken", "MTK",1000);
+        erc20.mint(msg.sender, 1000);
+        console.log("MyERC20 deployed to: %s", address(erc20));
         MyERC721 erc721 = new MyERC721("MyNFT", "MNFT");
+        console.log("MyERC721 deployed to: %s", address(erc721));
+        erc721.mint(msg.sender);
 
         vm.stopBroadcast();
     }
