9ac479f0a878eea8838b29b76ae54c01c758d003
minmeizi <minmz830@tydic.com>
2026-02-09 23:13:37 +0800

    erc20 test transfer

---
+++ script/TransferERC20.s.sol
@@ -0,0 +1,18 @@
+
+// script/TransferERC20.s.sol
+pragma solidity ^0.8.20;
+
+import "forge-std/Script.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
+contract TransferERC20 is Script {
+    function run() external {
+        address token = 0x7f52576Bf43A55F533972A238daa2E794cB9b87A;
+        address to = 0x00267645F5677350F740da9C8586Ded87816D811;
+        uint256 amount = 1e18;
+
+        vm.startBroadcast();
+        IERC20(token).transfer(to, amount);
+        vm.stopBroadcast();
+    }
+}
